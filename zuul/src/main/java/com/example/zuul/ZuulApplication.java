0f6e9b3687e4203e1a9533ade5ee1b7dfbe08c56
ChocolateAceCream <nuodi@hotmail.com>
2020-01-17 16:58:06 +0800

    add spring boot admin

--- zuul/src/main/java/com/example/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/example/zuul/ZuulApplication.java
@@ -1,11 +1,13 @@
 package com.example.zuul;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableAdminServer
 public class ZuulApplication {
 
     public static void main(String[] args) {
